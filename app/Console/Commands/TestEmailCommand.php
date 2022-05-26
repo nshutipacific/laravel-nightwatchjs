ce6ec2cb460cba6053134100bb3b9ca68fb5c9fb
Pacifique Nshuti <pacific.nsuti@zatechub.com>
2022-05-26 12:48:44 +0200

    Initial commit for testing side

---
+++ app/Console/Commands/TestEmailCommand.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Mail\TestEmail;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Mail;
+
+class TestEmailCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'test:mail';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        Mail::to('charlotteuwimbabazi2@gmail.com')->send(new TestEmail());
+    }
+}
